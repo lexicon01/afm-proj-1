38ee04a6722e945a276e744a268adc617244c738
unknown <ialex.roman@icloud.com>
2019-09-23 11:48:13 -0400

    'added

---
+++ js/instagram-funnel/scripts.js
@@ -0,0 +1,11 @@
+
+  
+
+  $(document).ready(function(){
+    $("#accordion").on("hide.bs.collapse show.bs.collapse", e => {
+        $(e.target)
+          .prev()
+          .find("i:last-child")
+          .toggleClass("fa-minus fa-plus");
+      });
+  });
